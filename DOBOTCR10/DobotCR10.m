896cadfaa10b922f7af754d35e179679c9d55dde
Muneeb2285 <145761314+Muneeb2285@users.noreply.github.com>
2023-10-29 14:00:06 +1100

    Add files via upload

---
+++ DOBOTCR10/DobotCR10.m
@@ -0,0 +1,40 @@
+classdef DobotCR10 < RobotBaseClass
+    %% Dobot CR10  created using technical sheet
+
+    properties(Access = public)              
+        plyFileNameStem = 'DobotCR10';
+    end
+    
+    methods
+%% Define robot Function 
+        function self = DobotCR10(baseTr)
+			self.CreateModel();
+            if nargin < 1			
+				baseTr = eye(4);				
+            end
+            self.model.base = self.model.base.T * baseTr * trotx(pi/2) * troty(pi/2);
+            
+            self.PlotAndColourRobot();         
+        end
+
+%% Create the robot model
+        function CreateModel(self)   
+            % Create the CR10 model by reading the technical manual
+           
+            link(1) = Link('d',0.1765,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]), 'offset',0);
+            link(2) = Link('d',0,'a',-0.607,'alpha',0,'qlim', deg2rad([-360 360]), 'offset',0);
+            link(3) = Link('d',0,'a',-0.568,'alpha',0,'qlim', deg2rad([-360 360]), 'offset', 0);
+            link(4) = Link('d',0.125,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
+            link(5) = Link('d',0.125,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
+            link(6) = Link('d',0.1114,'a',0,'alpha',0,'qlim',deg2rad([-360,360]), 'offset', 0);
+           
+        
+            link(2).offset = -pi/2;
+            link(4).offset = -pi/2;
+
+            
+            self.model = SerialLink(link,'name',self.name);
+        end
+     
+    end
+end
---
+++ DOBOTCR10/TestDobot.m
@@ -0,0 +1,35 @@
+function Lab3Starter()
+%% Lab3Starter Create a Puma 560 DH SerialLink Model
+% Although there is a Puma 560 Model in the toolbox, this code creates a model 
+% from the basic DH parameters that we derived in the previous theory exercise.  
+
+
+% Link('theta',__,'d',__,'a',__,'alpha',__,'offset',__,'qlim',[ ... ])
+
+            link(1) = Link('d',0.1765,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]), 'offset',0);
+            link(2) = Link('d',0,'a',-0.607,'alpha',0,'qlim', deg2rad([-360 360]), 'offset',0);
+            link(3) = Link('d',0,'a',-0.568,'alpha',0,'qlim', deg2rad([-360 360]), 'offset', 0);
+            link(4) = Link('d',0.125,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
+            link(5) = Link('d',0.125,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
+            link(6) = Link('d',0.1114,'a',0,'alpha',0,'qlim',deg2rad([-360,360]), 'offset', 0);
+           
+        
+            link(2).offset = -pi/2;
+            link(4).offset = -pi/2;
+
+
+
+myRobot = SerialLink([link(1) link(2) link(3) link(4) link(5) link(6) ], 'name', 'DobotCR10')
+
+q = zeros(1,6);
+
+myRobot.plot(q)
+
+myRobot.gravity
+myRobot.base
+myRobot.tool
+
+%% Teach
+myRobot.teach
+
+end
