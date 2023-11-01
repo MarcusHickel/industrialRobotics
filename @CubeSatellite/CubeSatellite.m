354e03817c55d036ef80711fc06fc0f81b4626f1
MarcusHickel <marcus.hickel@gmail.com>
2023-11-01 19:50:28 +1100

    Created CubeSat Class
    Using low ploy model

---
+++ @CubeSatellite/CubeSatellite.m
@@ -0,0 +1,54 @@
+classdef CubeSatellite < handle
+    %CUBESATELLITE 
+    %   Cube Satellite model
+    
+    properties
+        model;
+    end
+    
+ methods
+        %% ...structors
+        function self = CubeSatellite()
+            self.model = self.GetModel('cubesatA');
+            basePose = eye(4);
+            self.model.base = basePose;
+                
+            % Plot 3D model
+            plot3d(self.model,0,'workspace',[-2 2 -2 2 -2 2]);
+
+        end
+        
+        function delete(self)
+                handles = findobj('Tag', self.model.name);
+                h = get(handles,'UserData');
+                try delete(h.robot); end
+                try delete(h.wrist); end
+                try delete(h.link); end
+                try delete(h); end
+                try delete(handles); end
+        end
+end
+       
+    
+    methods (Static)
+        %% GetModel
+        function model = GetModel(name)
+            if nargin < 1
+                name = 'CubeSat';
+            end
+            [faceData,vertexData] = plyread('cubesatA.ply','tri');
+            link1 = Link('alpha',pi,'a',0,'d',0.005,'offset',0);
+            model = SerialLink(link1,'name',name);
+            
+            % Changing order of cell array from {faceData, []} to 
+            % {[], faceData} so that data is attributed to Link 1
+            % in plot3d rather than Link 0 (base).
+            model.faces = {[], faceData};
+
+            % Changing order of cell array from {vertexData, []} to 
+            % {[], vertexData} so that data is attributed to Link 1
+            % in plot3d rather than Link 0 (base).
+            model.points = {[], vertexData};
+        end
+    end    
+end
